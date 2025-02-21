eed8eb9e3dce3d34c2c5964335249506a06b1b51
Prashant Sengar <64002012+prashant-s99@users.noreply.github.com>
2025-02-21 15:52:20 +0530

    Server Fix

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 import axios from "axios";
 
-const BASE_URL = "https://192.168.1.6:1234";
+const BASE_URL = "https://localhost:1234";
 const CitiesContext = createContext();
 
 const initialCityState = {
