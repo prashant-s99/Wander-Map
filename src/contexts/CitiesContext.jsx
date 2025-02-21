2417663d65fbeb6eb1d69b639d8185b596e45280
Prashant Sengar <64002012+prashant-s99@users.noreply.github.com>
2025-02-21 17:06:31 +0530

    Update CitiesContext.jsx

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -7,7 +7,7 @@ import {
 } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:1234";
+const BASE_URL = "https://8196-106-219-122-50.ngrok-free.app";
 const CitiesContext = createContext();
 
 const initialCityState = {
